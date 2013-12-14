70f7ea6ac678126d4208010fd8125da53b7d75ac
Qin Liu <lqgy2001@gmail.com>
2013-12-14 13:39:13 +0800

    add c++ template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+/*
+ * Problem  : 
+ * Site     : 
+ * Category : 
+ * Status   : 
+ * Author   : Qin Liu (lqgy2001@gmail.com)
+ * Date     : 
+ * Remark   : N/A
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    return 0;
+}
