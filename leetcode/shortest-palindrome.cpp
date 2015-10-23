4d7e9f7bc72528ae56092d5d529b8bc0e4f9bcdd
Qin Liu <lqgy2001@gmail.com>
2015-10-23 15:53:19 +0800

    add solution

---
+++ leetcode/shortest-palindrome.cpp
@@ -0,0 +1,30 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        if (s == "")
+            return "";
+        int longest = 0;
+        for (int i = 0; i <= s.length()/2;) {
+            int begin, end;
+            begin = end = i;
+            while (end+1 < s.length() && s[end+1] == s[i])
+                end++;
+            i = end+1;
+            while (end+1 < s.length() && begin-1 >= 0 && s[end+1] == s[begin-1]) {
+                end++;
+                begin--;
+            }
+            if (begin == 0) {
+                int len = end+1;
+                longest = max(longest, len);
+            }
+        }
+        string p = s.substr(longest);
+        reverse(p.begin(), p.end());
+        return p + s;
+    }
+};
