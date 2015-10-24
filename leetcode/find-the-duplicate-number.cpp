5868843cee9a7080e8fd05e743682a2cd30f9055
Qin Liu <lqgy2001@gmail.com>
2015-10-24 19:03:55 +0800

    add Find the Duplicate Number

---
+++ leetcode/find-the-duplicate-number.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = 0;
+        for (auto& each : nums)
+            if (each > n)
+                n = each;
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = (left+right)/2;
+            int count = 0;
+            for (auto& each : nums)
+                if (each <= mid)
+                    count++;
+            if (count > mid)
+                right = mid;
+            else
+                left = mid+1;
+        }
+        return left;
+    }
+};
