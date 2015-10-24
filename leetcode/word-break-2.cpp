80a09f7699a6665cc61e7796ebbcf88bebcbfe1d
Qin Liu <lqgy2001@gmail.com>
2015-10-24 17:31:27 +0800

    add Word Break II

---
+++ leetcode/word-break-2.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <unordered_set>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void dfs(int start, string sentence, string& s, vector<vector<int>>& mark, vector<string>& results) {
+        if (start == s.length()) {
+            results.push_back(sentence);
+            return;
+        }
+        for (auto& end : mark[start]) {
+            string next = sentence + (sentence == "" ? s.substr(start, end-start) : " " + s.substr(start, end-start));
+            dfs(end, next, s, mark, results);
+        }
+    }
+    vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
+        vector<vector<int>> mark(s.length());
+        for (int end = s.length(); end > 0; end--) {
+            if (end < s.length() && mark[end].empty())
+                continue;
+            for (int start = end-1; start >= 0; start--)
+                if (wordDict.count(s.substr(start, end-start)))
+                    mark[start].push_back(end);
+        }
+        vector<string> results;
+        dfs(0, "", s, mark, results);
+        return results;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "catsanddog";
+    unordered_set<string> wordDict = {"cat", "cats", "and", "sand", "dog"};
+    vector<string> result = sol.wordBreak(s, wordDict);
+    for (auto& each : result)
+        cout << each << endl;
+}
