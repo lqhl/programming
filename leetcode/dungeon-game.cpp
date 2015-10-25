fe61403075caf327541656f4bf32784e2d452b3f
Qin Liu <lqgy2001@gmail.com>
2015-10-25 21:17:07 +0800

    add solutions
    - Find Median from Data Stream
    - Dungeon Game

---
+++ leetcode/dungeon-game.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int n = dungeon.size(), m = dungeon[0].size();
+        vector<vector<int>> dp(n, vector<int>(m));
+        for (int i = n-1; i >= 0; i--)
+            for (int j = m-1; j>=0; j--) {
+                int tmp = -1;
+                if (i < n-1)
+                    tmp = dp[i+1][j];
+                if (j < m-1 && (tmp == -1 || dp[i][j+1] < tmp))
+                    tmp = dp[i][j+1];
+                if (tmp == -1)
+                    tmp = 0;
+                if (dungeon[i][j] >= 0)
+                    dp[i][j] = max(0, tmp-dungeon[i][j]);
+                else
+                    dp[i][j] = -dungeon[i][j] + tmp;
+            }
+        return dp[0][0]+1;
+    }
+};
---
+++ leetcode/find-median-from-data-stream.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <queue>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+class MedianFinder {
+private:
+    int n;
+    int first;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    priority_queue<int> maxHeap;
+
+public:
+    MedianFinder() : n(0), minHeap(), maxHeap() { }
+
+    // Adds a number into the data structure.
+    void addNum(int num) {
+        n++;
+        if (n == 1)
+            first = num;
+        else if (n == 2) {
+            if (first > num)
+                swap(first, num);
+            maxHeap.push(first);
+            minHeap.push(num);
+        } else {
+            if (num <= maxHeap.top())
+                maxHeap.push(num);
+            else
+                minHeap.push(num);
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+            } else if (minHeap.size() > maxHeap.size() + 1) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+        }
+        cout << maxHeap.size() << ' ' << minHeap.size() << endl;
+    }
+
+    // Returns the median of current data stream
+    double findMedian() {
+        if (n == 1) return first;
+
+        if (minHeap.size() == maxHeap.size())
+            return (minHeap.top() + maxHeap.top()) * 0.5;
+        else if (minHeap.size() > maxHeap.size())
+            return minHeap.top();
+        else
+            return maxHeap.top();
+    }
+};
+
+int main() {
+    MedianFinder finder;
+    finder.addNum(1);
+    finder.addNum(2);
+    finder.addNum(3);
+    finder.addNum(4);
+    cout << finder.findMedian() << endl;
+}
