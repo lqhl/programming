a7240ac8fb5fe0e0e7b3cca2aedf0be4fd012294
Qin Liu <lqgy2001@gmail.com>
2015-10-28 11:01:52 +0800

    add Candy

---
+++ leetcode/candy.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int res = 1, prev = 1, last = 0, v = 1;
+        for (int i = 1; i < ratings.size(); i++) {
+            if (ratings[i] > ratings[i-1]) {
+                prev++;
+                res += prev;
+                last = i;
+                v = prev;
+            } else if (ratings[i] < ratings[i-1]) {
+                if (prev > 1) {
+                    prev = 1;
+                    res += prev;
+                } else {
+                    res += i-last + prev;
+                    if (v >= i-last+1)
+                        res--;
+                }
+            } else {
+                last = i;
+                if (prev > 1) {
+                    prev = 1;
+                    res += prev;
+                } else {
+                    res += prev;
+                }
+                v = prev;
+            }
+        }
+        return res;
+    }
+};
