25f3fd40a9660095d4e3c71eda8b3ff777dc436e
Qin Liu <lqgy2001@gmail.com>
2015-11-02 19:00:48 +0800

    add iterative solution

--- leetcode/binary-tree-postorder-traversal.cpp
+++ leetcode/binary-tree-postorder-traversal.cpp
@@ -10,6 +10,36 @@ struct TreeNode {
 };
 
 class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> results;
+        TreeNode* prev = nullptr;
+        stack<TreeNode*> s;
+        if (root)
+            s.push(root);
+        else
+            return results;
+        while (!s.empty()) {
+            TreeNode* cur = s.top();
+            if (prev == nullptr || prev->left == cur || prev->right == cur) {
+                if (cur->left)
+                    s.push(cur->left);
+                else if (cur->right)
+                    s.push(cur->right);
+            } else if (prev == cur->left) {
+                if (cur->right)
+                    s.push(cur->right);
+            } else {
+                results.push_back(cur->val);
+                s.pop();
+            }
+            prev = cur;
+        }
+        return results;
+    }
+};
+
+class Solution2 {
 private:
     void dfs(TreeNode* cur, vector<int>& result) {
         if (cur == nullptr) return;
