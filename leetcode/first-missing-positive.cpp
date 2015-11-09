31eed410404abd8086fb7f5da47d730ab457281c
Qin Liu <lqgy2001@gmail.com>
2015-11-09 16:50:22 +0800

    add solution

---
+++ leetcode/first-missing-positive.cpp
@@ -0,0 +1,17 @@
+#include "util.hpp"
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n;)
+            if (nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i])
+                swap(nums[nums[i]-1], nums[i]);
+            else
+                i++;
+        for (int i = 0; i < n; i++)
+            if (nums[i] != i+1)
+                return i+1;
+        return n+1;
+    }
+};
