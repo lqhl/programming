635a0cbfb31be13acce11b520447c386f0429dc7
Qin Liu <lqgy2001@gmail.com>
2015-12-02 17:27:05 +0800

    add solution

--- leetcode/distinct-subsequences.cpp
+++ leetcode/distinct-subsequences.cpp
@@ -1,5 +1,6 @@
 #include "util.hpp"
 
+// my own DP function
 class Solution {
 public:
     int numDistinct(string s, string t) {
@@ -15,5 +16,44 @@ public:
                 }
             }
         return dp2[m];
+    }
+};
+
+// standard
+class Solution2 {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.length(), m = t.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++) {
+                if (s[i-1] == t[j-1]) {
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        return dp[n][m];
+    }
+};
+
+// optimized for space
+class Solution3 {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.length(), m = t.length();
+        vector<int> dp(m+1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++)
+            for (int j = m; j >= 1; j--) {
+                if (s[i-1] == t[j-1]) {
+                    dp[j] = dp[j-1]+dp[j];
+                } else {
+                    dp[j] = dp[j];
+                }
+            }
+        return dp[m];
     }
 };
